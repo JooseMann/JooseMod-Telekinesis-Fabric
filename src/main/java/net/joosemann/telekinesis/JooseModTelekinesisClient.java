dfae648748980baaadc65adb1db865e93db308f3
JooseMann <justinkarate76@gmail.com>
2023-11-06 23:04:21 -0500

    Initial Commit

---
+++ src/main/java/net/joosemann/telekinesis/JooseModTelekinesisClient.java
@@ -0,0 +1,10 @@
+package net.joosemann.telekinesis;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class JooseModTelekinesisClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
