b085cff0fb2b1bad42ecc5491ad6ce107729628b
JooseMann <justinkarate76@gmail.com>
2023-11-11 10:23:45 -0500

    Added comments for clarity

--- src/main/java/net/joosemann/telekinesis/networking/packet/TelekinesisC2SPacket.java
+++ src/main/java/net/joosemann/telekinesis/networking/packet/TelekinesisC2SPacket.java
@@ -1,7 +1,6 @@
 package net.joosemann.telekinesis.networking.packet;
 
 import net.fabricmc.fabric.api.networking.v1.PacketSender;
-import net.joosemann.telekinesis.JooseModTelekinesisFabric;
 import net.joosemann.telekinesis.util.IEntityDataSaver;
 import net.joosemann.telekinesis.util.KeyInputHandler;
 import net.minecraft.nbt.NbtCompound;
@@ -9,19 +8,23 @@ import net.minecraft.network.PacketByteBuf;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.server.world.ServerWorld;
 import net.minecraft.text.Text;
 
 public class TelekinesisC2SPacket {
 
     public static NbtCompound playerNbt;
 
-    public static void onJoin(MinecraftServer server, ServerPlayerEntity player) {
+    public static void onJoin(ServerPlayerEntity player) {
         playerNbt = ((IEntityDataSaver) player).getPersistentData();
+
+        // Ensure that telekinesisIsActive is synced with the nbt data
+        KeyInputHandler.telekinesisIsActive = playerNbt.contains("telekinesis-enabled");
     }
 
+    // Although the parameters are unused, they have to be there for the packet to be registered correctly
     public static void receive(MinecraftServer server, ServerPlayerEntity player, ServerPlayNetworkHandler handler,
                                PacketByteBuf buf, PacketSender sender) {
+        // Flip telekinesisIsActive and the nbt data when the packet is sent (when the toggle telekinesis button is pressed)
 
         KeyInputHandler.telekinesisIsActive = !KeyInputHandler.telekinesisIsActive;
 
